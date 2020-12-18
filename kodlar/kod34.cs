06027e8ae6a2f851a3f0f8a1888dbe9a3326ff00
cmelisa <melisaceylan0101@gmail.com>
2020-12-18 17:20:02 +0300

    ders14 eklendi

---
+++ kodlar/kod34.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isim = new string[3];
+            int[] s1 = new int[3];
+            int[] s2 = new int[3];
+            int[] ort = new int[3];
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.Clear();
+                Console.Write((i + 1) + ". öğrencinin adı: ");
+                isim[i] = Console.ReadLine();
+
+                Console.Write((i + 1) + " sınav1: ");
+                s1[i] = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write((i + 1) + " sınav2: ");
+                s2[i] = Convert.ToInt32(Console.ReadLine());
+                ort[i] = ((s1[i] + s2[i]) / 2);
+            }
+            Console.WriteLine("öğrenci            sınav1          sınav2         ortalama");
+            Console.WriteLine();
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("   " + isim[i] + "  " + s1[i] + "    " + s2[i] + "     " + ort[i]);
+            }
+            Console.Read();
+
+        }
+    }
+}
---
+++ kodlar/kod35.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 5 ELEMANLI KLAVYEDEN ALDIĞIMIZ DEĞERLERİN EN BÜYÜĞÜNÜ YAZDIRMA
+            int[] sayilar = new int[5];
+            for(int i = 0; i < 5; i++)
+            {
+                Console.Write((i+1).ToString()+".sayı giriniz: ");
+                sayilar[i] =Convert.ToInt32( Console.ReadLine());
+
+            }
+            int enbuyuk;
+            enbuyuk = sayilar[0];
+            for (int i = 1; i < 5; i++)
+            {
+                if (enbuyuk < sayilar[i])
+                {
+                    enbuyuk = sayilar[i];
+                }
+            }
+            Console.WriteLine( enbuyuk+" girilen en buyuk değerdir.");
+            Console.Read();
+
+        }
+    }
+}
